8e59c51e421c53e3f4ee716038c8d4e5debcfa4c
Justin Li <justinlit23@gmail.com>
2022-04-11 13:56:54 -0700

    commit tree df923689540992bd47a71c805ff0828b92bbecac

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+const moveDodgerRight = () => {
+    const rightNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key=="ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener("keydown", (e) => {
+    if (e.key=="ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
